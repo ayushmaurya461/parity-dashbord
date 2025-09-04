998b118a484d34163288856e4b80a075eced088b
Ayush Maurya <ayush.maurya@dhwaniris.com>
2025-09-04 16:47:43 +0530

    change order

--- src/app/shared/config/environments.ts
+++ src/app/shared/config/environments.ts
@@ -5,11 +5,11 @@ export interface Environment {
 
 export const ENVIRONMENTS: Environment[] = [
   { title: 'Staging', url: 'https://devadminapi.mgrant.in' },
-  { title: 'Production', url: 'https://portaladminapi.mgrant.in' },
-  { title: 'UAT', url: 'https://uatadminapi.mgrant.in' },
   { title: 'QA', url: 'https://qaadminapi.mgrant.in' },
+  { title: 'UAT', url: 'https://uatadminapi.mgrant.in' },
   { title: 'ICICI UAT', url: 'https://uaticicifadminapi.mgrant.in' },
   { title: 'Sattva UAT', url: 'https://uatsattvaadminapi.mgrant.in' },
+  { title: 'Production', url: 'https://portaladminapi.mgrant.in' },
   { title: 'ICICI PROD', url: 'https://icicifadminapi.mgrant.in' },
   { title: 'Sattva PROD', url: 'https://sattvaadminapi.mgrant.in' },
   { title: 'TRIF PROD', url: 'https://trifmgrantadminapi.dhwaniris.com' },
